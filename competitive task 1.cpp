3f68b2c01af3f5c70dce5ae22eda70e75571fee3
shivansh70 <104889422+shivansh70@users.noreply.github.com>
2022-05-04 13:24:58 +0530

    Add files via upload

---
+++ competitive task 1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+   char str[] = "A string.";
+   char *point = str;
+
+   cout << str[0] <<' '<< *point <<' '<<point[3]<<"\n";
+   point+= 2;
+   cout <<*point<<' '<< point[2] <<' '<< point[5];
+
+   return 0;
+}
